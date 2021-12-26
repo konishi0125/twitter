432fa2ed560a1f4e1c57934a0acc409d5e8957f1
unknown <yumemita@gmail.com>
2021-12-27 07:47:11 +0900

    get my_favorite

---
+++ get_favorite.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import get_tweet
+
+
+data = pd.read_csv("./result/my_favorite.csv.csv")
+out = pd.DataFrame(get_tweet.get_tweet_by_favorite(2000), columns=["name", "text", "id"])
+out = out.astype({"id": np.int64})
+merge_data = pd.concat([out, data])
+
+result = merge_data[~merge_data.duplicated()]
+
+result.to_csv("./result/my_favorite.csv.csv", index=False)
+
