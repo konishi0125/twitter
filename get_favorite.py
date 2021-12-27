46f58da85ef2b8ddd2ddcfe13ea878b4bcd0dfb9
unknown <yumemita@gmail.com>
2021-12-27 13:02:16 +0900

    path fix

--- get_favorite.py
+++ get_favorite.py
@@ -3,12 +3,12 @@ import pandas as pd
 import get_tweet
 
 
-data = pd.read_csv("./result/my_favorite.csv.csv")
-out = pd.DataFrame(get_tweet.get_tweet_by_favorite(2000), columns=["name", "text", "id"])
+data = pd.read_csv("./result/my_favorite.csv")
+out = pd.DataFrame(get_tweet.get_tweet_by_favorite(), columns=["name", "text", "id"])
 out = out.astype({"id": np.int64})
 merge_data = pd.concat([out, data])
 
 result = merge_data[~merge_data.duplicated()]
 
-result.to_csv("./result/my_favorite.csv.csv", index=False)
+result.to_csv("./result/my_favorite.csv", index=False)
 
