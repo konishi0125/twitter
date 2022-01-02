629dc72189df9949e8f34b3a9c78d728afc9e97f
unknown <yumemita@gmail.com>
2022-01-02 11:35:46 +0900

    結果の確認

--- get_favorite.py
+++ get_favorite.py
@@ -4,11 +4,13 @@ import get_tweet
 
 
 data = pd.read_csv("./result/my_favorite.csv")
+before_length = len(data)
 out = pd.DataFrame(get_tweet.get_tweet_by_favorite(), columns=["name", "text", "id"])
 out = out.astype({"id": np.int64})
 merge_data = pd.concat([out, data])
 
 result = merge_data[~merge_data.duplicated()]
-
+after_length = len(result)
+print(f"before:{before_length}, after:{after_length}")
 result.to_csv("./result/my_favorite.csv", index=False)
 
