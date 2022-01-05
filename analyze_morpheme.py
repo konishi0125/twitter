043012e58f5aeb315478fa983ecf14a1bb66ced5
unknown <yumemita@gmail.com>
2022-01-05 09:36:23 +0900

    evaluate tweet

--- analyze_morpheme.py
+++ analyze_morpheme.py
@@ -15,30 +15,28 @@ favo_neutral = 0
 
 for index, sr in data.iterrows():
     text = sr["text"]
-    text_list = text.split("。")
-    for t in text_list:
-        if t == "":
-            continue
-        result = emotion_analyzer.analyze(t)
-        if len(result) == 2:
-            no_count += 1
-            continue
-        if result["orientation"] == "POSITIVE":
-            if sr["favorite"] is True:
-                favo_pos += 1
-            elif sr["favorite"] is False:
-                pos += 1
-        elif result["orientation"] == "NEGATIVE":
-            if sr["favorite"] is True:
-                favo_nega += 1
-            elif sr["favorite"] is False:
-                nega += 1
-        elif result["orientation"] == "NEUTRAL":
-            if sr["favorite"] is True:
-                favo_neutral += 1
-            elif sr["favorite"] is False:
-                neutral += 1
-
+    if text == "":
+        continue
+    result = emotion_analyzer.analyze(text)
+    if len(result) == 2:
+        no_count += 1
+        continue
+    if result["orientation"] == "POSITIVE":
+        if sr["favorite"] is True:
+            favo_pos += 1
+        elif sr["favorite"] is False:
+            pos += 1
+    elif result["orientation"] == "NEGATIVE":
+        if sr["favorite"] is True:
+            favo_nega += 1
+        elif sr["favorite"] is False:
+            nega += 1
+    elif result["orientation"] == "NEUTRAL":
+        print(result)
+        if sr["favorite"] is True:
+            favo_neutral += 1
+        elif sr["favorite"] is False:
+            neutral += 1
 
 sum = pos + nega + neutral
 favo_sum = favo_pos + favo_nega + favo_neutral
